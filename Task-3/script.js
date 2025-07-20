c2a2d62fe22251601db3a4ef7cfb5e155d1c589a
Shital Yadav <shitalyadav2395@gmail.com>
2025-07-20 19:40:21 +0530

    Add files via upload

---
+++ Task-3/script.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+function append(value) {
+  if (display.innerText === '0') {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = '0';
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText.replace('÷', '/').replace('×', '*'));
+  } catch {
+    display.innerText = 'Error';
+  }
+}
+
+function backspace() {
+  if (display.innerText.length === 1 || display.innerText === 'Error') {
+    display.innerText = '0';
+  } else {
+    display.innerText = display.innerText.slice(0, -1);
+  }
+}
